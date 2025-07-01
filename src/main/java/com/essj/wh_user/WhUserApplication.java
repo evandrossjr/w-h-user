bb628e6f1a28c7ee27b1489621e6f5f3201ce0a2
Evandro Sacramento II <evandro-dev@outlook.com>
2025-06-30 22:24:05 -0300

    primeiro commit

---
+++ src/main/java/com/essj/wh_user/WhUserApplication.java
@@ -0,0 +1,13 @@
+package com.essj.wh_user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhUserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhUserApplication.class, args);
+	}
+
+}
