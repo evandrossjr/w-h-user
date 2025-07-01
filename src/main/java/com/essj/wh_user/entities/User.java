6e9f9f1180352ddc2f9f215305061a158b99fe67
Evandro Sacramento II <evandro-dev@outlook.com>
2025-06-30 23:07:42 -0300

    "Criação de User e UserDTO"

---
+++ src/main/java/com/essj/wh_user/entities/User.java
@@ -0,0 +1,96 @@
+package com.essj.wh_user.entities;
+
+import com.essj.wh_user.dtos.UserDTO;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.util.Date;
+
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+    private String cpf;
+    private String endereco;
+    private String email;
+    private String telefone;
+    private Date dataCadastro;
+
+    public User() {
+    }
+
+    public User(String nome, String cpf, String endereco, String email, String telefone, Date dataCadastro) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.endereco = endereco;
+        this.email = email;
+        this.telefone = telefone;
+        this.dataCadastro = dataCadastro;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public Date getDataCadastro() {
+        return dataCadastro;
+    }
+
+    public void setDataCadastro(Date dataCadastro) {
+        this.dataCadastro = dataCadastro;
+    }
+
+    public static User fromDTO(UserDTO userDTO){
+        User user = new User();
+        user.setNome(userDTO.getNome());
+        user.setCpf(userDTO.getCpf());
+        user.setEndereco(userDTO.getEndereco());
+        user.setEmail(userDTO.getEmail());
+        user.setTelefone(userDTO.getTelefone());
+        user.setDataCadastro(userDTO.getDataCadastro());
+
+        return user;
+    }
+}
