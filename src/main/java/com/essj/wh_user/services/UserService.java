9696dbf280633e9635ee976b538236bd9965c5ab
Evandro Sacramento II <evandro-dev@outlook.com>
2025-07-03 22:58:22 -0300

    "Método findAll em UserService"

--- src/main/java/com/essj/wh_user/services/UserService.java
+++ src/main/java/com/essj/wh_user/services/UserService.java
@@ -1,7 +1,24 @@
 package com.essj.wh_user.services;
 
+import com.essj.wh_user.dtos.UserDTO;
+import com.essj.wh_user.entities.User;
+import com.essj.wh_user.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class UserService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    public List<UserDTO> findAll(){
+        List<User> users = userRepository.findAll();
+        return users.stream().map(UserDTO::toDTO).collect(Collectors.toList());
+
+        }
+
 }
